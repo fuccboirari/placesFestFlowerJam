8fbbdefd6ab3b50d5d640d6ee3a7b9f0ed4eedcf
fuccboirari <maks2394@gmail.com>
2020-05-15 01:56:23 +0500

    Lab_1

---
+++ MoscowTransport.WebService/DomainObjects/Fest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace placesFestFlowerJam.DomainObjects
+{
+    public class Fest : DomainObject
+    {
+        public string Address { get; set; }
+
+        public string PeriodOf { get; set; }
+
+        public string NumberTP { get; set; }
+
+        public string WorkWeekdays { get; set; }
+
+        public string WorkWeekend { get; set; }
+
+        public string WebSite { get; set; }
+    }
+}
---
+++ MoscowTransport.WebService/DomainObjects/Ports/IFestRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+namespace placesFestFlowerJam.DomainObjects.Ports
+{
+    public interface IReadOnlyFestRepository
+    {
+        Task<Fest> GetFest (long id);
+
+        Task<IEnumerable<Fest>> GetAllFests();
+
+        Task<IEnumerable<Fest>> QueryFests(ICriteria<Fest> criteria);
+
+    }
+
+    public interface IFestRepository
+    {
+        Task AddFest(Fest fest);
+
+        Task RemoveFest(Fest fest);
+
+        Task UpdateFest(Fest fest);
+    }
+}
