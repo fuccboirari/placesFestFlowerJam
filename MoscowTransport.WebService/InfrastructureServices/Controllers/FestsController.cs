04083f96c922751438a74b56ea22d7e26bed94a7
fuccboirari <maks2394@gmail.com>
2020-05-15 02:59:51 +0500

    Lab_3

---
+++ MoscowTransport.WebService/InfrastructureServices/Controllers/FestsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using placesFestFlowerJam.DomainObjects;
+
+namespace placesFestFlowerJam.WebService.InfrastructureServices.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FestsController : ControllerBase
+    {
+        private readonly ILogger<FestsController> _logger;
+
+        public FestsController(ILogger<FestsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Fest> GetAllRoutes()
+        {
+            return new List<Fest>()
+            {
+                new Fest()
+                {
+                    Id = 1,
+                    Address = "vl.19",
+                    PeriodOf = "22.08.2019-08.09.2019",
+                    NumberTP = "25",
+                    WorkWeekdays = "11-21",
+                    WorkWeekend = "10-22",
+                    Fests = new Fest()
+                    {
+                        Id = 1,
+                        Name = "Цветочный Джем",
+                        WebSite = "http://mos.ru"
+                    }
+                }
+            };
+        }
+    }
+}
+
---
+++ MoscowTransport.WebService/InfrastructureServices/Presenters/FestListPresenter.cs
@@ -0,0 +1,23 @@
+﻿using placesFestFlowerJam.ApplicationServices.GetRouteListUseCase;
+using System.Net;
+using Newtonsoft.Json;
+using placesFestFlowerJam.ApplicationServices.Ports;
+
+namespace placesFestFlowerJam.InfrastructureServices.Presenters
+{
+    public class FestListPresenter : IOutputPort<GetRouteListUseCaseResponse>
+    {
+        public JsonContentResult ContentResult { get; }
+
+        public FestListPresenter()
+        {
+            ContentResult = new JsonContentResult();
+        }
+
+        public void Handle(GetRouteListUseCaseResponse response)
+        {
+            ContentResult.StatusCode = (int)(response.Success ? HttpStatusCode.OK : HttpStatusCode.NotFound);
+            ContentResult.Content = response.Success ? JsonConvert.SerializeObject(response.Routes) : JsonConvert.SerializeObject(response.Message);
+        }
+    }
+}
